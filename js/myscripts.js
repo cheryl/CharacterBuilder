c29345c69b1d63416255b888d4a695667ea0e1fe
Cheryl <cheryl@chongsworship.com>
2011-02-18 18:28:55 +1300

    meerkat again

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
     $(".accordion").accordion({autoHeight: false, header: "h3"});
     $(".tabs").tabs();
+    $(".meerkat").hide();
 });
 
+$('#btn').click(function () {
+    $('.meerkat').slide();
+    /*
+    $('.meerkat').meerkat({
+        height: '100%',
+        width: '150px',
+        position: 'left',
+        close: '.close-meerkat',
+        animationIn: 'slide',
+        animationSpeed: 500,
+        easingIn: 'EaseIn'
+    });
+    */
+});
 
